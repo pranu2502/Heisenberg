f3526047d30d358e0380c3c791401e61ebe0e0e4
pranu2502 <52481227+pranu2502@users.noreply.github.com>
2020-01-10 22:01:35 +0530

    Add files via upload

--- heisenberg.py
+++ heisenberg.py
@@ -14,7 +14,7 @@ def speak(text):
     print(text)
     playsound.playsound("speak.mp3",True)
     os.system("rm speak.mp3")
-
+driver = ""
 command =""
 text = ""
 def getaudio():
@@ -39,7 +39,7 @@ def getaudio():
 
 def open_something():
     global command
-    
+    global driver
     if("google" in command):
         speak("What Can I google for you?")
         getaudio()
@@ -55,50 +55,52 @@ def open_something():
     elif("youtube" in command):
         speak("What do you want to search on youtube?")
         getaudio()
-        driver = webdriver.Chrome('/home/pranav/Downloads/chromedriver')
+        driver = webdriver.Chrome()
         driver.get("https://youtube.com/")
         search = driver.find_element_by_id("search")
         search.send_keys(command)
         search.send_keys(Keys.RETURN)
         
     elif(("whatsapp" or "watsapp") in command):
-        driver = webdriver.Chrome('/home/pranav/Downloads/chromedriver')
+        driver = webdriver.Chrome()
 	driver.get("https://web.whatsapp.com/")
 
     elif("gmail" in command):
-        driver = webdriver.Chrome('/home/pranav/Downloads/chromedriver')
+        driver = webdriver.Chrome()
 	driver.get("https://accounts.google.com/")
         username = driver.find_element_by_name("identifier")
         speak("Please enter your username")
         username.send_keys(raw_input("Username: "))
         username.send_keys(Keys.RETURN)
         speak("Please enter your password")
-        password = getpass.getpass()
+        password = driver.find_element_by_name("password")
+        password.send_keys(getpass.getpass())
         password.send_keys(Keys.RETURN)
     
     elif("lms" in command):
-        driver = webdriver.Chrome('/home/pranav/Downloads/chromedriver')
+        driver = webdriver.Chrome()
         driver.get("https://lms.iiitb.ac.in/moodle/login/index.php")
         username = driver.find_element_by_name("username")
         speak("Please enter your username")
         username.send_keys(raw_input("Username: "))
         speak("Please enter your password")
         password = driver.find_element_by_id("password")
-        password = getpass.getpass()
-        password.send_keys(Keys.RETURN)
+        password.send_keys(getpass.getpass())
+        username.send_keys(Keys.RETURN)
     elif("facebook" in command):
-        driver = webdriver.Chrome('/home/pranav/Downloads/chromedriver')
+        driver = webdriver.Chrome()
 	driver.get("https://www.facebook.com/")
-        username = driver.find_element_by_name("username")
+        username = driver.find_element_by_name("email")
         speak("Please enter your username")
         username.send_keys(raw_input("Username: "))
         speak("Please enter your password")
-        password = driver.find_element_by_id("password")
-        password = getpass.getpass()
+        password = driver.find_element_by_id("pass")
+        password.send_keys(getpass.getpass())
         password.send_keys(Keys.RETURN)
 
 def play():
     global command
+    global driver
     if("music" in command):
 	speak("From where do you want me to play music?")
 	getaudio()
@@ -125,7 +127,7 @@ def play():
 
 def send():
     global command
-    
+    global driver
     if("message" in command):
 	if("facebook" in command):
             driver = webdriver.Chrome()
@@ -135,7 +137,7 @@ def send():
             username.send_keys(raw_input())
             password = driver.find_element_by_id("pass")
             speak("Plase enter your password after I stop")
-            password.send_keys(raw_input())
+            password.send_keys(getpass.getpass())
             password.send_keys(Keys.RETURN)
         elif("whatsapp" or "watsapp" in command):
             driver = webdriver.Chrome()
@@ -149,7 +151,8 @@ def send():
         username.send_keys(raw_input("Username: "))
         username.send_keys(Keys.RETURN)
         speak("Please enter your password")
-        password = getpass.getpass()
+        password = driver.find_element_by_id("password")
+        password.send_keys(getpass.getpass())
         password.send_keys(Keys.RETURN)
         compose = driver.find_element_by_Xpath('//*[@id=:ku]/div/div')
         compose.click()
@@ -173,17 +176,17 @@ def send():
             getaudio()
             message.send_keys(text)
 
-
 def find():
     global command
-    driver = webdriver.Chrome('/home/pranav/Downloads/chromedriver')
+    global driver
+    driver = webdriver.Chrome()
     if("eat" in command):
         driver.get("https://www.google.com/search?q=places+to+eat+nearby")
     elif("fun" in command):
         driver.get("https://www.google.com/search?q=fun+nearby")
     
 def navigate():
-    print("yes")
+    global driver
     global command
     speak("What is your starting point")
     getaudio()
@@ -191,18 +194,21 @@ def navigate():
     speak("Where do you want to go?")
     getaudio()
     command2 = command
-    driver = webdriver.Chrome('/home/pranav/Downloads/chromedriver')
+    driver = webdriver.Chrome()
     driver.get("https://www.google.com/maps/dir/" + command1 + "/" + command2)
 
 k = 0
 
 def start():
+    global driver
     global k
     speak("Hello, I am Heisenberg, what can I do for you?")
     while(k == 0):
         getaudio()
         if("open" in command):
             open_something()
+ #       elif("where am i" or "can you spot my location")
+            
         elif("play" in command):
             play()
         elif("send" in command):
@@ -214,24 +220,21 @@ def start():
         elif("who are you" in command):
             speak("Hello, I am Heisenberg")
         elif("search" in command):
-            driver = webdriver.Chrome('/home/pranav/Downloads/chromedriver')
+            driver = webdriver.Chrome()
             driver.get("https://www.google.com/search?q=" + command)
 	elif(("find" or "nearby") in command):
             find()
         elif(("read" or "news" )in command):
-            driver = webdriver.Chrome('/home/pranav/Downloads/chromedriver')
+            driver = webdriver.Chrome()
             driver.get("https://news.google.com/")
     	elif("navigate" in command):
             navigate()
-        elif(("close" or "bye" or "seeya" or "see you next time" or "until next time") in command):
+        elif(("close" or "bye bye" or "bye bye bye" or "seeya" or "see you next time" or "until next time") in command):
             k = 1
         else:
-	    driver = webdriver.Chrome('/home/pranav/Downloads/chromedriver')
+	    driver = webdriver.Chrome()
 	    driver.get("https://www.google.com/search?q=" + command)
 
 
+
 start()
-
-
-
-
